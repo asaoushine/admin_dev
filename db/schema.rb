16b5f6a02271d838e4103daca9d6b134a251aa01
asao <asaoushine6@gmail.com>
2015-04-15 13:50:44 +0900

    ユーザーページ完成。ここまでで一旦admin_devを閉じる。
    次は投稿画面などと合わせる。

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150413030849) do
+ActiveRecord::Schema.define(version: 20150415023852) do
 
   create_table "articles", force: :cascade do |t|
     t.integer  "user_id",    limit: 4
@@ -24,6 +24,11 @@ ActiveRecord::Schema.define(version: 20150413030849) do
   end
 
   add_index "articles", ["user_id"], name: "index_articles_on_user_id", using: :btree
+
+  create_table "change_passwords", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  limit: 255, default: "", null: false
